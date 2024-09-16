1fa58bd17a89f08494cd87655abfeba2b3e76c24
Ross Waguespack <rosswaguespack@3e-7d-99-cd-80-60.wlan.lsu.edu>
2024-09-16 12:26:57 -0500

    Warhol

---
+++ sketch.js
@@ -0,0 +1,63 @@
+function setup() {
+    createCanvas(400, 269);
+    background(100);
+    Comment
+    fill(200)
+    square (0,0,400,)
+
+    fill(0)
+    strokeWeight(3)
+    rect(0, 255, 140, 50);
+
+    ellipse(0, 0, 40, 40)
+    ellipse(0, 10, 20, 40)
+    ellipse(0, 30, 30, 10)
+
+    strokeWeight(4)
+    line(0, 110, 120, 110)
+    stroke('white')
+    strokeWeight(12)
+    line (30,5, 95, 5)
+    strokeWeight(12)
+    line (0,120, 110, 120)
+    strokeWeight(4)
+    line (0,195, 130, 195)
+    stroke('Black')
+    strokeWeight(7)
+    line(0, 190, 130, 190)
+
+    strokeWeight(6)
+    line(100, 0, 150, 300)
+    strokeWeight(6)
+    line(140, 0, 190, 300)
+    
+
+    strokeWeight(18)
+    line (360,0, 390, 400);
+
+    strokeWeight(12)
+    line (360,7, 410, 100);
+
+    stroke('white')
+    strokeWeight(4)
+    line (340,0, 395, 150);
+
+    stroke('white')
+    strokeWeight(9)
+    line (320,0, 417, 300);
+
+    stroke('black')
+    strokeWeight(4)
+    line (325,0, 360, 100);
+
+    stroke('black')
+    strokeWeight(7)
+    line (162, 130, 290, 270);
+    strokeWeight(9)
+    line (245, 210, 295, 270);
+    
+}
+
+function draw() {
+
+}
